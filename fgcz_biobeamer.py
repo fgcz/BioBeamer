1d159517a0742d1f59c5902d560207c81ec44609
Christian Panse <cpanse@users.noreply.github.com>
2015-02-23 14:41:04 +0100

    Update fgcz_biobeamer.py
    variable refactor

--- fgcz_biobeamer.py
+++ fgcz_biobeamer.py
@@ -51,14 +51,14 @@ class BioBeamer(object):
             sys.exit(1)
 
         hdlr = logging.FileHandler(self.para['logging_file'])
-        hdlr_syslog21 = logging.handlers.SysLogHandler(address=('130.60.81.148', 514))
+        hdlr_syslog = logging.handlers.SysLogHandler(address=('130.60.81.148', 514))
 
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
 
         hdlr.setFormatter(formatter)
-        hdlr_syslog21.setFormatter(formatter)
+        hdlr_syslog.setFormatter(formatter)
         self.logger.addHandler(hdlr)
-        self.logger.addHandler(hdlr_syslog21)
+        self.logger.addHandler(hdlr_syslog)
         self.logger.setLevel(logging.INFO)
 
     def print_para(self):
