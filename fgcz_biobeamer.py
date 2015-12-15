f063d4b3e4db997618e7147e76cd01024af2e16a
Christian Panse <cpanse@users.noreply.github.com>
2015-12-15 09:01:09 +0100

    Update fgcz_biobeamer.py

--- fgcz_biobeamer.py
+++ fgcz_biobeamer.py
@@ -6,11 +6,22 @@ $HeadURL: http://fgcz-svn.unizh.ch/repos/fgcz/stable/proteomics/BioBeamer/fgcz_b
 $Id: fgcz_biobeamer.py 7229 2015-02-05 09:46:15Z cpanse $
 $Date: 2015-02-05 10:46:15 +0100 (Thu, 05 Feb 2015) $
 
-Copyright 2004-2015
+Copyright 2006-2015 Functional Genomics Center Zurich
 
-Christian Panse <cp@fgcz.ethz.ch>
-Christian Trachsel <christian.trachsel@fgcz.uzh.ch>
-Witold E. Wolski <wew@fgcz.ethz.ch>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+Author / Maintainer: Christian Panse <cp@fgcz.ethz.ch>
 """
 
 import os
