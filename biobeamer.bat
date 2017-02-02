cd2575cca649989d869ca7e606588380ba63e68f
Witold Wolski <wewolski@gmail.com>
2017-02-02 12:13:34 +0100

    Update biobeamer.bat

--- biobeamer.bat
+++ biobeamer.bat
@@ -1,2 +1,2 @@
 git -C C:\FGCZ\BioBeamer\ pull
-c:\python27\python.exe C:\FGCZ\BioBeamer\biobeamer2.py file:///c:/FGCZ/BioBeamer IWJpbzA3YmVhbWVyIQ==
+c:\python27\python.exe C:\FGCZ\BioBeamer\biobeamer2.py file:///c:/FGCZ/BioBeamer
