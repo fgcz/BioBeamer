751afd6105478361690db97d3241c606efa12c25
Witold Wolski <wewolski@gmail.com>
2017-03-13 15:10:54 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,9 @@
-git clone -b biobeamer2 https://github.com/fgcz/BioBeamer
+rem install git
 rem install python
 rem install pycharm
+mkdir c:/fgcz/
+cd c:/fgcz/
+git clone -b biobeamer2 https://github.com/fgcz/BioBeamer
 
 /c/Python27/python get-pip.py
 /c/Python27/Scripts/pip install lxml==3.6.0
