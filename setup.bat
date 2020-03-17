93dd8cc1f963cd2879de2233a2ea71260976940c
administrator@FGCZ-NET.unizh.ch <administrator@FGCZ-NET.unizh.ch>
2020-03-17 18:50:05 +0100

    fgcz-r-032

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@ rem install python
 rem install pycharm
 mkdir c:/fgcz/
 cd c:/fgcz/
-git clone -b biobeamer2 https://github.com/fgcz/BioBeamer
+git clone https://github.com/fgcz/BioBeamer
 
 /c/Python27/python get-pip.py
 /c/Python27/Scripts/pip install lxml==3.6.0
