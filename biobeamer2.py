e22a960b7c2578d6dd1a6b524dcc4b0f045f0e87
Witold Wolski <wewolski@gmail.com>
2020-04-16 12:01:30 +0200

    [REF] removed two unused variables from MyLog.

--- biobeamer2.py
+++ biobeamer2.py
@@ -22,22 +22,18 @@ class MyLog:
     def __init__(self, name="BioBeamer"):
         self.logger = logging.getLogger(name)
         self.formatter = logging.Formatter('%(name)s - %(asctime)s - %(levelname)s - %(message)s')
-        self.is_syshandler = False
-        self.is_filehandler = False
 
     def add_file(self, filename="./log/biobeamer.log", level=logging.DEBUG):
-        if not self.is_syshandler:
-            file_handler = logging.FileHandler(filename)
-            file_handler.setLevel(level)
-            file_handler.setFormatter(self.formatter)
-            self.logger.addHandler(file_handler)
+        file_handler = logging.FileHandler(filename)
+        file_handler.setLevel(level)
+        file_handler.setFormatter(self.formatter)
+        self.logger.addHandler(file_handler)
 
     def add_syshandler(self, address=("130.60.81.148", 514), level=logging.DEBUG):
-        if not self.is_filehandler:
-            syslog_handler = logging.handlers.SysLogHandler(address=address)
-            syslog_handler.setLevel(level)
-            syslog_handler.setFormatter(self.formatter)
-            self.logger.addHandler(syslog_handler)
+        syslog_handler = logging.handlers.SysLogHandler(address=address)
+        syslog_handler.setLevel(level)
+        syslog_handler.setFormatter(self.formatter)
+        self.logger.addHandler(syslog_handler)
 
     def set_log_level(self, level=logging.INFO):
         self.logger.setLevel(level)
