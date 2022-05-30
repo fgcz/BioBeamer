d8045624fb45027263108211a4f2abc332f8602c
unknown <Administrator@fgcz-k-335.FGCZ-NET.unizh.ch>
2022-05-30 12:25:53 +0200

    removed robocopy /z option

--- biobeamer2.py
+++ biobeamer2.py
@@ -153,9 +153,9 @@ def robocopy_exec(file_to_copy,
     file_copied = None
 
     if not mov:
-        robocopy_args = "/E /Z /NP /R:0 /LOG+:{log}".format(log=logfile)
+        robocopy_args = "/E /NP /R:0 /LOG+:{log}".format(log=logfile)
     else:
-        robocopy_args = "/E /Z /NP /R:0 /MOV /LOG+:{log}".format(log=logfile)
+        robocopy_args = "/E /NP /R:0 /MOV /LOG+:{log}".format(log=logfile)
 
     cmd = [
         "robocopy.exe",
