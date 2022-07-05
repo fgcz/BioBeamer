dcbe7fbd2a90ee6dda18db110ed5c842f041da18
localanalytic <localanalytic@fgcz-k-092.FGCZ-NET.unizh.ch>
2022-07-05 16:06:35 +0200

    mapping function for Ultraflex

--- mapping_functions.py
+++ mapping_functions.py
@@ -2,6 +2,28 @@ import os
 import re
 from datetime import date
 
+def map_data_ultraflex(dest_path, logger):
+    """
+    input: \\\\fgcz-biobeamer.uzh.ch\\Data2SAN\\p65\\Proteomics\\ULTRAFLEXTREME\\analytic_20200924\\D_Eris_22708
+    output: \\\\fgcz-biobeamer.uzh.ch\\Data2San\\p22708\\Proteomics\\ULTRAFLEXTREME\\analytic_20200924_D_Eris_22708\\
+    """
+
+    pattern_dest = "^\\\\\\\\fgcz-biobeamer.uzh.ch\\\\Data2San\\\\orders\\\\(Proteomics|Metabolomics)\\\\[A-Z]{1,20}_[0-9]{1,2}\\\\(C[0-9]{3,6})_[a-z]{1,30}_[0-9]{8}\S[^%]+$"
+    #"[-0-9a-zA-Z\\_\/\.]" #does not match with.
+    regex_dest = re.compile(pattern_dest)
+    match_dest = regex_dest.match(dest_path)
+
+    if match_dest:
+        container_id = match_dest.group(2)
+        order_id = container_id.replace("C", "p")
+        dest_path = dest_path.replace("p65", order_id)
+        dest_path = dest_path.replace(container_id + "_", "")
+        dest_path = os.path.normpath(dest_path)
+        return dest_path
+    else:
+        return dest_path
+
+
 def map_data_rapiflex(dest_path, logger):
     """
     input: \\\\fgcz-biobeamer.uzh.ch\\Data2San\\orders\\Proteomics\\RAPIFLEX_1\\C28830_nanni_20220701\\LN_1568
