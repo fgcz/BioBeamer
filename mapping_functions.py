5d77bb70ce8cacfe6d9402215d6aad1d593a22ee
unknown <Rapiflex_1@Flex-PC.FGCZ-NET.unizh.ch>
2022-07-05 09:53:27 +0200

    First attempt Rapiflex

--- mapping_functions.py
+++ mapping_functions.py
@@ -2,6 +2,29 @@ import os
 import re
 from datetime import date
 
+def map_data_rapiflex(dest_path, logger):
+    """
+    input: \\\\fgcz-biobeamer.uzh.ch\\Data2San\\orders\\Proteomics\\RAPIFLEX_1\\C28830_nanni_20220701\\LN_1568
+    output: \\\\fgcz-biobeamer.uzh.ch\\Data2San\\p28830\\Proteomics\\RAPIFLEX_1\\nanni_20201021\\N_1568
+    """
+
+    pattern_dest = "^\\\\\\\\fgcz-biobeamer.uzh.ch\\\\Data2San\\\\orders\\\\(Proteomics|Metabolomics)\\\\[A-Z]{1,20}_[0-9]{1,2}\\\\(C[0-9]{3,6})_([a-z]{1,30})_([0-9]{8})[_0-9A-Za-z]*\\\\[-0-9a-zA-Z_\/\.\\]+$"
+    regex_dest = re.compile(pattern_dest)
+    match_dest = regex_dest.match(dest_path)
+
+    if match_dest:
+        container_id = match_dest.group(2)
+        username = match_dest.group(3)
+        date = match_dest.group(4)
+        order_id = container_id.replace("C", "p")
+        dest_path = dest_path.replace("orders", order_id)
+        dest_path = dest_path.replace(container_id + "_", "")
+        dest_path = os.path.normpath(dest_path)
+        return dest_path
+    else:
+        return dest_path
+
+
 def map_data_G2HD_2(path, logger):
     """
     input:
