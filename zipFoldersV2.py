993772e5d42ccdfd128a518cb9178231a1ab8549
wewolski@gmail.com <wewolski@gmail.com>
2023-10-05 16:02:01 +0200

    updated settings on timstof Flex

--- zipFoldersV2.py
+++ zipFoldersV2.py
@@ -5,20 +5,6 @@ import zipfile
 import shutil
 import sys
 
-if len(sys.argv) == 2:
-    directory = sys.argv[1]
-    depth = sys.argv[2]
-else:
-    directory = "A:\\Large_images"
-    depth = 5
-
-if len(sys.argv) > 2:
-    outdir = sys.argv[3]
-else:
-    outdir = os.path.join("A:\\", "Data2San_IMG")
-
-
-
 
 # find all directories to zip
 def get_dirs_plus_depth(walk, directory, depth=0):
@@ -45,8 +31,6 @@ def zipdir(src, zip_name):
     return src
 
 
-
-
 def handleRemoveReadonly(func, path, exc):
     excvalue = exc[1]
     if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
@@ -60,29 +44,45 @@ def remove(dir):
     print("removing: " + dir)
     shutil.rmtree(dir, ignore_errors=False, onerror=handleRemoveReadonly)
 
-walk = [x[0] for x in os.walk(directory)]
 
-print(len(get_dirs_plus_depth(walk, directory, depth=0)) == 1)
-dirs2zip = get_dirs_plus_depth(walk, directory, depth=depth)
+if __name__ == '__main__':
+    if sys.maxsize > 2 ** 32:
+        print("python is 64")
 
-# create output locations
-zip_files = [os.path.join(outdir, x.replace(directory + "\\", "")) + ".zip" for x in dirs2zip]
+    if len(sys.argv) >= 2:
+        directory = sys.argv[1]
+        depth = int(sys.argv[2])
+    else:
+        directory = "A:\\Large_images"
+        depth = 5
 
-# create output directories
-zip_dir_uniq = list(set([os.path.dirname(x) for x in zip_files]))
-zip_dir_to_create = [x for x in zip_dir_uniq if not os.path.exists(x)]
-[os.makedirs(x) for x in zip_dir_to_create]
+    if len(sys.argv) > 2:
+        outdir = sys.argv[3]
+    else:
+        outdir = os.path.join("A:\\", "Data2San_IMG")
 
-tmp = list(map(zipdir, dirs2zip, zip_files))
+    walk = [x[0] for x in os.walk(directory)]
 
+    print(len(get_dirs_plus_depth(walk, directory, depth=0)) == 1)
+    dirs2zip = get_dirs_plus_depth(walk, directory, depth=depth)
 
-folders2remove = set([os.path.dirname(x) for x in dirs2zip])
-script_file_path = os.path.join(directory, "rm_files.sh")
-with open(script_file_path, "a" if os.path.exists(script_file_path) else "w") as script_file:
-    for file_path in folders2remove:
-        script_file.write(f"rm -Rf \"{file_path}\"\n")
+    # create output locations
+    zip_files = [os.path.join(outdir, x.replace(directory + "\\", "")) + ".zip" for x in dirs2zip]
 
-#tmp = list(map(remove, folders2remove))
+    # create output directories
+    zip_dir_uniq = list(set([os.path.dirname(x) for x in zip_files]))
+    zip_dir_to_create = [x for x in zip_dir_uniq if not os.path.exists(x)]
+    [os.makedirs(x) for x in zip_dir_to_create]
+
+    tmp = list(map(zipdir, dirs2zip, zip_files))
+
+    folders2remove = set([os.path.dirname(x) for x in dirs2zip])
+    script_file_path = os.path.join(directory, "rm_files.sh")
+    with open(script_file_path, "a" if os.path.exists(script_file_path) else "w") as script_file:
+        for file_path in folders2remove:
+            script_file.write(f"rm -Rf \"{file_path}\"\n")
+
+    tmp = list(map(remove, folders2remove))
 
 
 
