897e8f0fb76fc73b2cc20c9bd85e5560968ee5d4
Claudio Cannizzaro <claudio.cannizzaro@fgcz.uzh.ch>
2025-06-05 16:47:34 +0200

    Refactor Drive class in mapNetworks.py; improve code readability and formatting

--- src/mapNetworks.py
+++ src/mapNetworks.py
@@ -4,44 +4,60 @@ import subprocess
 
 
 class Drive:
-    def __init__(self, logger, networkPath="\\\\fgcz-ms.fgcz-net.unizh.ch\\Data2San",
-                 user="FGCZ-NET\BioBeamer",
-                 password="cGFzc3dvcmQ="):
+    def __init__(
+        self,
+        logger,
+        networkPath="\\\\fgcz-ms.fgcz-net.unizh.ch\\Data2San",
+        user=r"FGCZ-NET\BioBeamer",
+        password="cGFzc3dvcmQ=",
+    ):
         self._networkPath = networkPath
         self._user = user
         self._password = base64.b64decode(password)
         self._password = self._password.decode("utf-8")
         self._logger = logger
 
-
     def mapDrive(self):
         winCMD = "net use {networkPath} {password} /user:{user}".format(
-            networkPath=self._networkPath,
-            password=self._password,
-            user=self._user)
+            networkPath=self._networkPath, password=self._password, user=self._user
+        )
 
-        self._logger.info("Attempt: net use {networkPath} <password> /user:{user}.".format(networkPath=self._networkPath,
-                                                                                           user=self._user))
+        self._logger.info(
+            "Attempt: net use {networkPath} <password> /user:{user}.".format(
+                networkPath=self._networkPath, user=self._user
+            )
+        )
 
-        p = subprocess.Popen(winCMD, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        p = subprocess.Popen(
+            winCMD, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+        )
 
         out, err = p.communicate()
         err = err.decode("utf-8")
         out = out.decode("utf-8")
         if not err == "":
-            #self._logger.error(winCMD)
-            self._logger.info("Network drive {} for user {} can NOT be MAPPED.".format(self._networkPath, self._user))
+            # self._logger.error(winCMD)
+            self._logger.info(
+                "Network drive {} for user {} can NOT be MAPPED.".format(
+                    self._networkPath, self._user
+                )
+            )
             self._logger.error(err.replace("\r\n", " "))
         else:
             self._logger.info(out.replace("\r\n", " "))
-            self._logger.info("Network drive {} for user {} mapped without error code.".format(self._networkPath, self._user))
+            self._logger.info(
+                "Network drive {} for user {} mapped without error code.".format(
+                    self._networkPath, self._user
+                )
+            )
         return p.returncode
 
     def unmapDrive(self):
-        winCMD = "net use {networkPath} /delete".format(
-            networkPath=self._networkPath)
+        winCMD = "net use {networkPath} /delete".format(networkPath=self._networkPath)
         self._logger.info(winCMD)
-        p = subprocess.Popen(winCMD, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        p = subprocess.Popen(
+            winCMD, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+        )
         out, err = p.communicate()
         err = err.decode("utf-8")
         out = out.decode("utf-8")
