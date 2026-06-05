bcde2d4cc8e88276f6599d3eca982d64d2135e2b
Witold Wolski <wewolski@gmail.com>
2026-06-05 15:45:34 +0200

    Fix setup.bat: clone p37 instead of deleted biobeamer2 branch

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@ rem install python
 rem install pycharm
 mkdir c:/fgcz/
 cd c:/fgcz/
-git clone -b biobeamer2 https://github.com/fgcz/BioBeamer
+git clone -b p37 https://github.com/fgcz/BioBeamer
 
 /c/Python39/python get-pip.py
 /c/Python39/Scripts/pip install lxml==3.6.0
